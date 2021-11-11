ee8ccce8cf0eb9241bd7ca716214a4f4dc4fcbba
polizhur <polizhur@gmail.com>
2021-11-11 10:25:23 +0100

    Set up GET, DELETE DELETE/id endpoints for entries

--- routers/entry.js
+++ routers/entry.js
@@ -4,6 +4,26 @@ const Entry = require("../models").entry;
 
 const router = new Router();
 
+// GET /entries/        fetch all entries for user
+// POST /entries/       create new entry
+// DELETE /entries/     delete all entries for user
+// DELETE /entries/:id  delete entry with id "id"
+
+router.get("/", authMiddleware, async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const entries = await Entry.findAll({
+      where: {
+        userId: userId,
+      },
+      order: [["createdAt", "DESC"]],
+    });
+    res.status(200).send({ entries });
+  } catch (e) {
+    next(e);
+  }
+});
+
 router.post("/", authMiddleware, async (req, res, next) => {
   try {
     const userId = req.user.id;
@@ -23,6 +43,43 @@ router.post("/", authMiddleware, async (req, res, next) => {
       userId,
     });
     res.status(200).send({ message: "Mood added", newEntry });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.delete("/:id", authMiddleware, async (req, res, next) => {
+  try {
+    const entryId = parseInt(req.params.id);
+    const specificEntry = await Entry.findByPk(entryId);
+
+    const loggedInUserId = req.user.id;
+    const entryUserId = specificEntry.userId;
+
+    if (loggedInUserId != entryUserId) {
+      return res
+        .status(400)
+        .send({ message: "Entry does not belong to this user" });
+    }
+
+    specificEntry.destroy();
+    res.status(200).send({ message: "Mood deleted" });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.delete("/", authMiddleware, async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+
+    await Entry.destroy({
+      where: {
+        userId: userId,
+      },
+    });
+
+    res.status(200).send({ message: "All moods deleted" });
   } catch (e) {
     next(e);
   }
