40280d6af050379028faffeae1e889702fa409af
rlcngz <erolcengiz@gmail.com>
2021-11-11 14:26:28 +0100

    Removed sentiment API. Only used Giphy.

--- routers/entry.js
+++ routers/entry.js
@@ -5,43 +5,26 @@ const authMiddleware = require("../auth/middleware");
 
 const router = new Router();
 
-router.post("/sentiment", async (req, res) => {
-  const { content } = req.body;
-  const options = {
-    method: "GET",
-    url: "https://twinword-emotion-analysis-v1.p.rapidapi.com/analyze/",
-    params: {
-      text: content,
-    },
-    headers: {
-      "x-rapidapi-host": "twinword-emotion-analysis-v1.p.rapidapi.com",
-      "x-rapidapi-key": "1063711ffamsh31af34f715bef9dp14c537jsnf568a3552ef0",
-    },
-  };
-
-  axios
-    .request(options)
-    .then(function (response) {
-      const sentiment = response.data.emotions_detected[0];
-
-      axios
-        .get(
-          `https://api.giphy.com/v1/gifs/search?q=${sentiment}&api_key=UGwSvXyX5MwOX64tVf2q6KF4X9pXrrJV`
-        )
-        .then(function (response) {
-          const randomObj =
-            response.data.data[
-              Math.floor(Math.random() * response.data.data.length) - 1
-            ].images.original.url;
-          res.send(randomObj);
-        })
-        .catch(function (error) {
-          console.error(error);
-        });
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
+router.post("/gif", async (req, res, next) => {
+  try {
+    const { sentiment } = req.body;
+    const getGif = await axios
+      .get(
+        `https://api.giphy.com/v1/gifs/search?q=${sentiment}&api_key=UGwSvXyX5MwOX64tVf2q6KF4X9pXrrJV`
+      )
+      .then(function (response) {
+        const randomObj =
+          response.data.data[
+            Math.floor(Math.random() * response.data.data.length) - 1
+          ].images.original.url;
+        res.send(randomObj);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  } catch (e) {
+    next(e);
+  }
 });
 
 // GET /entries/        fetch all entries for user
